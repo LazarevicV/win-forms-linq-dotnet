6d632594bdd4029489e15389a0a4f6b0fd3b32c6
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-03 20:40:23 +0200

    added new form

---
+++ DrugiKolokvijumskiZadatak/frmNewOrder.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace DrugiKolokvijumskiZadatak
+{
+    partial class frmNewOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.dgvOrderDetails = new System.Windows.Forms.DataGridView();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.txtDiscount = new System.Windows.Forms.TextBox();
+            this.txtQuantity = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.btnAddItem = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblQuantity = new System.Windows.Forms.Label();
+            this.cmbProduct = new System.Windows.Forms.ComboBox();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.btnAddOrder = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbShipper = new System.Windows.Forms.ComboBox();
+            this.lblShipper = new System.Windows.Forms.Label();
+            this.cmbCustomer = new System.Windows.Forms.ComboBox();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.cmbEmployee = new System.Windows.Forms.ComboBox();
+            this.lblEmployee = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(538, 431);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(91, 34);
+            this.btnSave.TabIndex = 45;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(653, 431);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 34);
+            this.btnCancel.TabIndex = 44;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // dgvOrderDetails
+            // 
+            this.dgvOrderDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrderDetails.Location = new System.Drawing.Point(61, 188);
+            this.dgvOrderDetails.Name = "dgvOrderDetails";
+            this.dgvOrderDetails.RowHeadersWidth = 51;
+            this.dgvOrderDetails.RowTemplate.Height = 24;
+            this.dgvOrderDetails.Size = new System.Drawing.Size(686, 227);
+            this.dgvOrderDetails.TabIndex = 43;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(196, 113);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(38, 16);
+            this.lblPrice.TabIndex = 42;
+            this.lblPrice.Text = "Price";
+            // 
+            // txtDiscount
+            // 
+            this.txtDiscount.Location = new System.Drawing.Point(475, 133);
+            this.txtDiscount.Name = "txtDiscount";
+            this.txtDiscount.Size = new System.Drawing.Size(121, 22);
+            this.txtDiscount.TabIndex = 41;
+            // 
+            // txtQuantity
+            // 
+            this.txtQuantity.Location = new System.Drawing.Point(336, 133);
+            this.txtQuantity.Name = "txtQuantity";
+            this.txtQuantity.Size = new System.Drawing.Size(121, 22);
+            this.txtQuantity.TabIndex = 40;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(199, 133);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(121, 22);
+            this.txtPrice.TabIndex = 39;
+            // 
+            // btnAddItem
+            // 
+            this.btnAddItem.Location = new System.Drawing.Point(620, 134);
+            this.btnAddItem.Name = "btnAddItem";
+            this.btnAddItem.Size = new System.Drawing.Size(127, 23);
+            this.btnAddItem.TabIndex = 38;
+            this.btnAddItem.Text = "Add New Item";
+            this.btnAddItem.UseVisualStyleBackColor = true;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(472, 114);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(59, 16);
+            this.lbl.TabIndex = 37;
+            this.lbl.Text = "Discount";
+            // 
+            // lblQuantity
+            // 
+            this.lblQuantity.AutoSize = true;
+            this.lblQuantity.Location = new System.Drawing.Point(333, 114);
+            this.lblQuantity.Name = "lblQuantity";
+            this.lblQuantity.Size = new System.Drawing.Size(55, 16);
+            this.lblQuantity.TabIndex = 36;
+            this.lblQuantity.Text = "Quantity";
+            // 
+            // cmbProduct
+            // 
+            this.cmbProduct.FormattingEnabled = true;
+            this.cmbProduct.Location = new System.Drawing.Point(61, 133);
+            this.cmbProduct.Name = "cmbProduct";
+            this.cmbProduct.Size = new System.Drawing.Size(121, 24);
+            this.cmbProduct.TabIndex = 35;
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(58, 114);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(53, 16);
+            this.lblProduct.TabIndex = 34;
+            this.lblProduct.Text = "Product";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(475, 39);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(44, 16);
+            this.lblDate.TabIndex = 33;
+            this.lblDate.Text = "DATE";
+            // 
+            // btnAddOrder
+            // 
+            this.btnAddOrder.Location = new System.Drawing.Point(620, 32);
+            this.btnAddOrder.Name = "btnAddOrder";
+            this.btnAddOrder.Size = new System.Drawing.Size(127, 23);
+            this.btnAddOrder.TabIndex = 32;
+            this.btnAddOrder.Text = "Add New Order";
+            this.btnAddOrder.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(475, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Date";
+            // 
+            // cmbShipper
+            // 
+            this.cmbShipper.FormattingEnabled = true;
+            this.cmbShipper.Location = new System.Drawing.Point(336, 31);
+            this.cmbShipper.Name = "cmbShipper";
+            this.cmbShipper.Size = new System.Drawing.Size(121, 24);
+            this.cmbShipper.TabIndex = 30;
+            // 
+            // lblShipper
+            // 
+            this.lblShipper.AutoSize = true;
+            this.lblShipper.Location = new System.Drawing.Point(333, 12);
+            this.lblShipper.Name = "lblShipper";
+            this.lblShipper.Size = new System.Drawing.Size(54, 16);
+            this.lblShipper.TabIndex = 29;
+            this.lblShipper.Text = "Shipper";
+            // 
+            // cmbCustomer
+            // 
+            this.cmbCustomer.FormattingEnabled = true;
+            this.cmbCustomer.Location = new System.Drawing.Point(199, 31);
+            this.cmbCustomer.Name = "cmbCustomer";
+            this.cmbCustomer.Size = new System.Drawing.Size(121, 24);
+            this.cmbCustomer.TabIndex = 28;
+            // 
+            // lblCustomer
+            // 
+            this.lblCustomer.AutoSize = true;
+            this.lblCustomer.Location = new System.Drawing.Point(196, 12);
+            this.lblCustomer.Name = "lblCustomer";
+            this.lblCustomer.Size = new System.Drawing.Size(64, 16);
+            this.lblCustomer.TabIndex = 27;
+            this.lblCustomer.Text = "Customer";
+            // 
+            // cmbEmployee
+            // 
+            this.cmbEmployee.FormattingEnabled = true;
+            this.cmbEmployee.Location = new System.Drawing.Point(61, 31);
+            this.cmbEmployee.Name = "cmbEmployee";
+            this.cmbEmployee.Size = new System.Drawing.Size(121, 24);
+            this.cmbEmployee.TabIndex = 26;
+            // 
+            // lblEmployee
+            // 
+            this.lblEmployee.AutoSize = true;
+            this.lblEmployee.Location = new System.Drawing.Point(58, 12);
+            this.lblEmployee.Name = "lblEmployee";
+            this.lblEmployee.Size = new System.Drawing.Size(69, 16);
+            this.lblEmployee.TabIndex = 25;
+            this.lblEmployee.Text = "Employee";
+            // 
+            // frmNewOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 485);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.dgvOrderDetails);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.txtDiscount);
+            this.Controls.Add(this.txtQuantity);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.btnAddItem);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.lblQuantity);
+            this.Controls.Add(this.cmbProduct);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.btnAddOrder);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbShipper);
+            this.Controls.Add(this.lblShipper);
+            this.Controls.Add(this.cmbCustomer);
+            this.Controls.Add(this.lblCustomer);
+            this.Controls.Add(this.cmbEmployee);
+            this.Controls.Add(this.lblEmployee);
+            this.MaximumSize = new System.Drawing.Size(816, 532);
+            this.Name = "frmNewOrder";
+            this.Text = "frmNewOrder";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrderDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.DataGridView dgvOrderDetails;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox txtDiscount;
+        private System.Windows.Forms.TextBox txtQuantity;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Button btnAddItem;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblQuantity;
+        private System.Windows.Forms.ComboBox cmbProduct;
+        private System.Windows.Forms.Label lblProduct;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Button btnAddOrder;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbShipper;
+        private System.Windows.Forms.Label lblShipper;
+        private System.Windows.Forms.ComboBox cmbCustomer;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.ComboBox cmbEmployee;
+        private System.Windows.Forms.Label lblEmployee;
+    }
+}
