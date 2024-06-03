fe100ac2e2997fc09246d12ec042093c08376b91
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-03 20:39:57 +0200

    starter form

---
+++ DrugiKolokvijumskiZadatak/frmMain.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace DrugiKolokvijumskiZadatak
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReset = new System.Windows.Forms.Button();
+            this.cmbProduct = new System.Windows.Forms.ComboBox();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.cmbCustomer = new System.Windows.Forms.ComboBox();
+            this.lblCustomer = new System.Windows.Forms.Label();
+            this.cmbEmployee = new System.Windows.Forms.ComboBox();
+            this.lblEmployee = new System.Windows.Forms.Label();
+            this.btnNewOrder = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(470, 37);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 22;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // cmbProduct
+            // 
+            this.cmbProduct.FormattingEnabled = true;
+            this.cmbProduct.Location = new System.Drawing.Point(332, 36);
+            this.cmbProduct.Name = "cmbProduct";
+            this.cmbProduct.Size = new System.Drawing.Size(121, 24);
+            this.cmbProduct.TabIndex = 21;
+            this.cmbProduct.SelectedIndexChanged += new System.EventHandler(this.cmbProduct_SelectedIndexChanged);
+            // 
+            // lblProduct
+            // 
+            this.lblProduct.AutoSize = true;
+            this.lblProduct.Location = new System.Drawing.Point(329, 17);
+            this.lblProduct.Name = "lblProduct";
+            this.lblProduct.Size = new System.Drawing.Size(53, 16);
+            this.lblProduct.TabIndex = 20;
+            this.lblProduct.Text = "Product";
+            // 
+            // cmbCustomer
+            // 
+            this.cmbCustomer.FormattingEnabled = true;
+            this.cmbCustomer.Location = new System.Drawing.Point(196, 36);
+            this.cmbCustomer.Name = "cmbCustomer";
+            this.cmbCustomer.Size = new System.Drawing.Size(121, 24);
+            this.cmbCustomer.TabIndex = 19;
+            this.cmbCustomer.SelectedIndexChanged += new System.EventHandler(this.cmbCustomer_SelectedIndexChanged);
+            // 
+            // lblCustomer
+            // 
+            this.lblCustomer.AutoSize = true;
+            this.lblCustomer.Location = new System.Drawing.Point(193, 17);
+            this.lblCustomer.Name = "lblCustomer";
+            this.lblCustomer.Size = new System.Drawing.Size(64, 16);
+            this.lblCustomer.TabIndex = 18;
+            this.lblCustomer.Text = "Customer";
+            // 
+            // cmbEmployee
+            // 
+            this.cmbEmployee.FormattingEnabled = true;
+            this.cmbEmployee.Location = new System.Drawing.Point(58, 36);
+            this.cmbEmployee.Name = "cmbEmployee";
+            this.cmbEmployee.Size = new System.Drawing.Size(121, 24);
+            this.cmbEmployee.TabIndex = 17;
+            this.cmbEmployee.SelectedIndexChanged += new System.EventHandler(this.cmbEmployee_SelectedIndexChanged);
+            // 
+            // lblEmployee
+            // 
+            this.lblEmployee.AutoSize = true;
+            this.lblEmployee.Location = new System.Drawing.Point(55, 17);
+            this.lblEmployee.Name = "lblEmployee";
+            this.lblEmployee.Size = new System.Drawing.Size(69, 16);
+            this.lblEmployee.TabIndex = 16;
+            this.lblEmployee.Text = "Employee";
+            // 
+            // btnNewOrder
+            // 
+            this.btnNewOrder.Location = new System.Drawing.Point(891, 28);
+            this.btnNewOrder.Name = "btnNewOrder";
+            this.btnNewOrder.Size = new System.Drawing.Size(96, 38);
+            this.btnNewOrder.TabIndex = 15;
+            this.btnNewOrder.Text = "New Order";
+            this.btnNewOrder.UseVisualStyleBackColor = true;
+            this.btnNewOrder.Click += new System.EventHandler(this.btnNewOrder_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(52, 76);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(943, 380);
+            this.dataGrid.TabIndex = 14;
+            this.dataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellContentClick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 480);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.cmbProduct);
+            this.Controls.Add(this.lblProduct);
+            this.Controls.Add(this.cmbCustomer);
+            this.Controls.Add(this.lblCustomer);
+            this.Controls.Add(this.cmbEmployee);
+            this.Controls.Add(this.lblEmployee);
+            this.Controls.Add(this.btnNewOrder);
+            this.Controls.Add(this.dataGrid);
+            this.MinimumSize = new System.Drawing.Size(1055, 527);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ComboBox cmbProduct;
+        private System.Windows.Forms.Label lblProduct;
+        private System.Windows.Forms.ComboBox cmbCustomer;
+        private System.Windows.Forms.Label lblCustomer;
+        private System.Windows.Forms.ComboBox cmbEmployee;
+        private System.Windows.Forms.Label lblEmployee;
+        private System.Windows.Forms.Button btnNewOrder;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ DrugiKolokvijumskiZadatak/frmMain.cs
@@ -0,0 +1,156 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrugiKolokvijumskiZadatak
+{
+    public partial class frmMain : Form
+    {
+        OrderBL orderBl;
+        ProductBL productBl;
+        CustomerBL customerBl;
+        EmployeeBL employeeBl;
+        public frmMain()
+        {
+            InitializeComponent();
+            orderBl = new OrderBL();
+            productBl = new ProductBL();
+            customerBl = new CustomerBL();
+            employeeBl = new EmployeeBL();
+        }
+
+        private void SearchOrders()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+
+            int? selectedEmployeeId = null;
+            if (cmbEmployee.SelectedIndex != -1 && cmbEmployee.SelectedValue != null && cmbEmployee.SelectedValue is int)
+            {
+                selectedEmployeeId = (int)cmbEmployee.SelectedValue;
+            }
+
+            string selectedCustomerId = cmbCustomer.SelectedIndex != -1 ? cmbCustomer.SelectedValue.ToString() : null;
+
+            int? selectedProductId = null;
+            if (cmbProduct.SelectedIndex != -1 && cmbProduct.SelectedValue != null && cmbProduct.SelectedValue is int)
+            {
+                selectedProductId = (int)cmbProduct.SelectedValue;
+            }
+
+            var results = db.spSearchOrders(selectedEmployeeId, selectedCustomerId, selectedProductId).ToList();
+            dataGrid.DataSource = results;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnNewOrder_Click(object sender, EventArgs e)
+        {
+            frmNewOrder fo = new frmNewOrder();
+            fo.ShowDialog();
+            dataGrid.DataSource = orderBl.GetAllOrders();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cmbEmployee.SelectedIndex = -1;
+            cmbCustomer.SelectedIndex = -1;
+            cmbProduct.SelectedIndex = -1;
+        }
+
+        private void cmbCustomer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SearchOrders();
+        }
+
+        private void cmbProduct_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SearchOrders();
+        }
+
+        private void cmbEmployee_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SearchOrders();
+        }
+
+        private void DataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // Display preview button for order details
+            DataGridViewButtonColumn btnColumnPreview = new DataGridViewButtonColumn();
+            btnColumnPreview.HeaderText = "Order Details";
+            btnColumnPreview.Text = "Preview";
+            btnColumnPreview.UseColumnTextForButtonValue = true; // Display text on button
+            btnColumnPreview.Name = "OrderDetails"; // Set the column name
+            dataGrid.Columns.Add(btnColumnPreview);
+
+            //Display edit button for order editing
+            DataGridViewButtonColumn btnColumnEdit = new DataGridViewButtonColumn();
+            btnColumnEdit.HeaderText = "Edit";
+            btnColumnEdit.Text = "Edit";
+            btnColumnEdit.UseColumnTextForButtonValue = true; // Display text on button
+            btnColumnEdit.Name = "Edit"; // Set the column name
+            dataGrid.Columns.Add(btnColumnEdit);
+
+            // Populate grid with data
+            dataGrid.DataSource = orderBl.GetAllOrders();
+
+            //Populate combo boxes
+            cmbEmployee.DataSource = employeeBl.GetAllEmployees();
+            cmbEmployee.DisplayMember = "FirstName";
+            cmbEmployee.ValueMember = "EmployeeID";
+            cmbEmployee.SelectedIndex = -1;
+
+            cmbCustomer.DataSource = customerBl.getAllCustomers();
+            cmbCustomer.DisplayMember = "CompanyName";
+            cmbCustomer.ValueMember = "CustomerID";
+            cmbCustomer.SelectedIndex = -1;
+
+            cmbProduct.DataSource = productBl.getAllProducts();
+            cmbProduct.DisplayMember = "ProductName";
+            cmbProduct.ValueMember = "ProductID";
+            cmbProduct.SelectedIndex = -1;
+        }
+
+        private void dataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int orderId = (int)dataGrid.Rows[e.RowIndex].Cells["OrderID"].Value;
+            // Handle preview order button click
+            if (e.ColumnIndex == dataGrid.Columns["OrderDetails"].Index && e.RowIndex >= 0)
+            {
+                frmPreviewOrderDetail detailsForm = new frmPreviewOrderDetail(orderId);
+                detailsForm.ShowDialog();
+
+
+            }
+            else if (e.ColumnIndex == dataGrid.Columns["Edit"].Index && e.RowIndex >= 0)
+            {
+                frmEditOrder editOrderForm = new frmEditOrder(orderId);
+                editOrderForm.ShowDialog();
+                dataGrid.DataSource = orderBl.GetAllOrders();
+            }
+            else
+            {
+                frmEditOrder editOrderForm = new frmEditOrder(orderId);
+                editOrderForm.ShowDialog();
+                dataGrid.DataSource = orderBl.GetAllOrders();
+            }
+
+        }
+    }
+}
