a62e459e6c1bf322df95eec990f36cd4fa78b006
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-03 19:40:14 +0200

    Add project files.

---
+++ DataLayer/CustomerDL.cs
@@ -0,0 +1,26 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    internal class CustomerDL
+    {
+        private readonly NorthwindEntities _context;
+
+        public CustomerDL()
+        {
+            _context = new NorthwindEntities();
+        }
+
+        public List<CustomerDTO> GetAllCustomers()
+        {
+            var customers = _context.Customers.ToList();
+
+            return Mapper.convertToList(customers);
+        }
+    }
+}
---
+++ Domain/GetOrdersResultsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class GetOrdersResultsDTO
+    {
+        public int OrderID { get; set; }
+        public System.Nullable<System.DateTime> OrderDate { get; set; }
+        public string CompanyName { get; set; }
+        public string Customer { get; set; }
+        public System.Nullable<decimal> TotalPrice { get; set; }
+    }
+}
