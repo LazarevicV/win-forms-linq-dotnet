7e7c40acc6de48e6ab4f3c912046e7e659561187
LazarevicV <vukadinlazarevic@gmail.com>
2024-06-03 20:40:36 +0200

    added preview order detail form

---
+++ DrugiKolokvijumskiZadatak/frmPreviewOrderDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrugiKolokvijumskiZadatak
+{
+    public partial class frmPreviewOrderDetail : Form
+    {
+        int orderDetailID;
+        public frmPreviewOrderDetail(int orderDetailId)
+        {
+            InitializeComponent();
+
+            orderDetailID = orderDetailId;
+        }
+
+        private void frmPreviewOrderDetail_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
